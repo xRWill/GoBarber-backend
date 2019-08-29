8c4c82d2c36f2b75901c3aad9a0aee7b328d59c9
xRWill <50185092+xRWill@users.noreply.github.com>
2019-08-29 20:04:43 -0300

    Aula - Configurando MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,6 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
 import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
@@ -10,14 +12,23 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
+
     // For all itens in array models load init() with connection
     models.map(model => model.init(this.connection));
     models.map(
       model => model.associate && model.associate(this.connection.models)
+    );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true }
     );
   }
 }
